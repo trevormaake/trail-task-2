f42e8a17feac646e6651490072709feabb02ff72
Trevor Maake <67583183+trevormaake@users.noreply.github.com>
2023-08-25 15:10:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const customCursor = document.getElementById('custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+    customCursor.style.left = e.clientX + 'px';
+    customCursor.style.top = e.clientY + 'px';
+});
